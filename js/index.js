b486cb431867ba0a3da752e71fa7c1e1db35039f
KAYITARE-C <cyntkayitare@gmail.com>
2022-10-07 11:02:47 +0200

    add

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-let typed=new Typed(".typing",{
-	strings:["Web Designer","Web Developer","Graphic Designer","Computer Repair Technician"],
-	typeSpeed:100,
-	backSpeed:60,
-	loop:true
-})
+let typed = new Typed(".typing", {
+  strings: ["Web Designer", "Web Developer", "UI/UX Designer","Computer Repair"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
