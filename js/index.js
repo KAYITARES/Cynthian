ab2faec30ee56eee42ac412b06de01029f999e27
KAYITARE-C <cyntkayitare@gmail.com>
2022-11-04 11:03:54 +0200

    add contact

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 let typed = new Typed(".typing", {
-  strings: ["Web Designer", "Web Developer", "UI/UX Designer","Computer Repair"],
+  strings: [
+    "Web Designer",
+    "Web Developer",
+    "UI/UX Designer",
+    "Computer Repair",
+  ],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
 });
+let year = new Date().getFullYear();
+document.querySelector(".getYear").textContent = year;
+console.log(year);
